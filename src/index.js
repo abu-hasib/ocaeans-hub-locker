69dd44b7ba596de3d9ded5f076584618cdc4f1c2
Ridwan <ricklick262@gmail.com>
2021-03-01 15:33:26 +0100

    express, babel, nodemon, dotenv setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import logOut from './ano.js'
-import sep from "path"
+import logOut from "./ano.js";
+import cors from 'cors';
 
-console.log('Hello ever running Node.js project.')
-logOut()
+import sep from "path";
+
+console.log("Hello ever running Node.js project.");
+logOut();
 console.log(process.env.MY_SECRET);
+
+import express from "express";
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/success', (req, res) => {
+  res.send('navigation to the rental page is successful');
+});
+
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
