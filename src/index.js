b8a32b4a62679567fe072b6cddaedb934136e8c4
Ridwan <ricklick262@gmail.com>
2021-03-04 00:06:35 +0100

    Initialize react setup

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-import logOut from "./ano.js";
-import cors from 'cors';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-import sep from "path";
-
-console.log("Hello ever running Node.js project.");
-logOut();
-console.log(process.env.MY_SECRET);
-
-import express from "express";
-
-const app = express();
-
-app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-app.get('/success', (req, res) => {
-  res.send('navigation to the rental page is successful');
-});
-
-app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.svg$/,
+        loader: "svg-inline-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist"),
+    hot: true,
+  },
+};
